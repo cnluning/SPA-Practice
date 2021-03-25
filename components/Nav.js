dc4b47962ffbd837e72698dd8108363ac5824570
Claire Luning <cnluning@gmail.com>
2021-03-24 21:32:03 -0500

    include code from class 9.3

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,33 @@
-import html from "html-literal";
-export default (links) => html`
-  <nav>
-    <i class="fas fa-bars"></i>
-    <ul class="hidden--mobile nav-links">
-      ${links.reduce(
-        (html, link) =>
-          html +
-          `<li><a href="#" title="${link.title}" data-navigo>${link.text}</a></li>`,
-        ``
-      )}
-    </ul>
-  </nav>
+export default (links) => `
+<nav>
+  <i class="fas fa-bars"></i>
+  <ul class="hidden--mobile nav-links">
+  ${links
+    .map(
+      (link) =>
+        `<li><a href="/${link.title !== "Home" ? link.title : ""}" title="${
+          link.title
+        }" data-navigo>${link.text}</a></li>`
+    )
+    .join("")}
+  </ul>
+</nav>
 `;
+
+// import html from "html-literal";
+// export default (links) => html`
+//   <nav>
+//     <i class="fas fa-bars"></i>
+//     <ul class="hidden--mobile nav-links">
+//       ${links.reduce(
+//         (html, link) =>
+//           html +
+//           `<li><a href="#" title="${link.title}" data-navigo>${link.text}</a></li>`,
+//         ``
+//       )}
+//     </ul>
+//   </nav>
+// `;
 
 //* <li><a href="#bio">Bio</a></li>
 //*<li><a href="#gallery">Gallery</a></li>
--- index.js
+++ index.js
@@ -8,6 +8,18 @@ import capitalize from "lodash";
 
 const router = new Navigo(window.location.origin);
 
+router.hooks({
+  before: (done, params) => {
+    // Because not all routes pass params we have to guard against is being undefined
+    const page =
+      params && Object.prototype.hasOwnProperty.call(params, "page")
+        ? capitalize(params.page)
+        : "Home";
+    fetchDataByView(state[page]);
+    done();
+  },
+});
+
 router
   .on({
     "/": () => {
@@ -90,7 +102,7 @@ function addNavEventListeners() {
     );
 }
 function addPicOnFormSubmit(st) {
-  if (st.view === "Form") {
+  if (st.page === "Form") {
     document.querySelector("form").addEventListener("submit", (event) => {
       event.preventDefault();
       // convert HTML elements to Array
@@ -106,5 +118,49 @@ function addPicOnFormSubmit(st) {
       state.Gallery.pictures.push(newPic);
       render(state.Gallery);
     });
+  }
+  if (st.page === "Order") {
+    document.querySelector("form").addEventListener("submit", (event) => {
+      event.preventDefault();
+      console.log(event);
+      const inputList = event.target.elements;
+      const toppings = [];
+      for (let input of inputList.toppings) {
+        if (input.checked) {
+          toppings.push(input.value);
+        }
+      }
+      const requestData = {
+        crust: inputList.crust.value,
+        cheese: inputList.cheese.value,
+        sauce: inputList.sauce.value,
+        toppings: toppings,
+      };
+      axios
+        .post(`http://localhost:4040/pizzas`, requestData)
+        .then((response) => {
+          state.Pizza.pizzas.push(response.data);
+          router.navigate("/Pizza");
+        })
+        .catch((error) => {
+          console.log("It puked", error);
+        });
+    });
+  }
+}
+
+function fetchDataByView(st = state.Home) {
+  switch (st.page) {
+    case "Pizza":
+      axios
+        .get(`http://localhost:4040/pizzas`)
+        .then((response) => {
+          state[st.page].pizzas = response.data;
+          render(st);
+        })
+        .catch((error) => {
+          console.log("It puked", error);
+        });
+      break;
   }
 }
